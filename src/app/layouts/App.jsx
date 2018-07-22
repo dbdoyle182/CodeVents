1d7755293b76637cc37f1492bd0518c4686794eb
Daniel Doyle <dbdoyle182@gmail.com>
2018-07-22 19:13:43 -0400

    created semantic ui navbar

--- src/app/layouts/App.jsx
+++ src/app/layouts/App.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
-import { Button } from 'semantic-ui-react';
+import { Container } from 'semantic-ui-react';
 import EventDashBoard from '../../features/event/EventDashboard/EventDashboard';
-
+import NavBar from '../../features/nav/navBar/NavBar';
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Re-vents</h1>
-        <EventDashBoard/>
+        <NavBar/>
+        <Container className="main">
+          <EventDashBoard/>
+        </Container>
       </div>
     );
   }
