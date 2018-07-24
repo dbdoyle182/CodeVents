96808820ddd84430a5c8c98dab37208b1974af39
Daniel Doyle <dbdoyle182@gmail.com>
2018-07-24 16:30:10 -0400

    adding events to mainpage

---
+++ src/features/event/eventList/EventList.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import EventListItem from './EventListItem';
+export default class EventList extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Event List</h1>
+        <EventListItem />
+        <EventListItem />
+        <EventListItem />
+      </div>
+    )
+  }
+}
---
+++ src/features/event/eventList/EventListAttendee.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class EventListAttendee extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Event List Attendee</h1>
+      </div>
+    )
+  }
+}
