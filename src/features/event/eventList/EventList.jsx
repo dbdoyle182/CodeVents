8cc8af142e32bd30618877c41c8fddc2a6ec0bec
Daniel Doyle <dbdoyle182@gmail.com>
2018-07-24 18:23:07 -0400

    adding props

--- src/features/event/eventList/EventList.jsx
+++ src/features/event/eventList/EventList.jsx
@@ -2,12 +2,15 @@ import React, { Component } from 'react'
 import EventListItem from './EventListItem';
 export default class EventList extends Component {
   render() {
+    const {events} = this.props.events;
     return (
       <div>
         <h1>Event List</h1>
+        {events.map((event) => (
+          <EventListItem key={event.id} event={event} />
+        ))}
         <EventListItem />
-        <EventListItem />
-        <EventListItem />
+      
       </div>
     )
   }
