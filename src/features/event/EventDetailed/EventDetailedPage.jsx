3da9dcdc84d08b1911d796bc3efe53365c236bcf
Daniel Doyle <dbdoyle182@gmail.com>
2018-07-29 16:18:17 -0400

    added additional files/folders

---
+++ src/features/event/EventDetailed/EventDetailedPage.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class EventDetailedPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Event Detailed Page</h1>
+      </div>
+    )
+  }
+}
