d4242207e25503fa6044b0f36ce8f5fc59a621c6
Daniel Doyle <dbdoyle182@gmail.com>
2018-08-06 22:50:59 -0400

    added some complex form input components

--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -5,6 +5,9 @@ import { reduxForm, Field } from 'redux-form';
 import { createEvent, updateEvent } from '../eventActions';
 import cuid from 'cuid';
 import TextInput from '../../../app/common/form/TextInput';
+import TextArea from '../../../app/common/form/TextArea';
+import SelectInput from '../../../app/common/form/SelectInput';
+
 
 
 
@@ -32,12 +35,22 @@ const actions = {
   createEvent,
   updateEvent
 }
+
+const category = [
+    {key: 'drinks', text: 'Drinks', value: 'drinks'},
+    {key: 'culture', text: 'Culture', value: 'culture'},
+    {key: 'film', text: 'Film', value: 'film'},
+    {key: 'food', text: 'Food', value: 'food'},
+    {key: 'music', text: 'Music', value: 'music'},
+    {key: 'travel', text: 'Travel', value: 'travel'},
+];
+
 class EventForm extends Component {
   constructor(props) {
     super(props)
-  
+
     this.state = {
-      event: {...this.props.event}
+      
     }
   }
 
@@ -57,27 +70,34 @@ class EventForm extends Component {
     }
   }
 
-  onInputChange = (event) => {
-    const newEvent = this.state.event
-    newEvent[event.target.name] = event.target.value;
-    
-    this.setState({
-      event: newEvent
-    })
-   
-  }
   render() {
-    const {handleCancel} = this.props;
-    const {event} = this.state;
+    
     return (
             <Grid>
               <Grid.Column width={10}>
                 <Segment>
                   <Header sub color='teal' content='Event Details'/>
                   <Form>
-                    <Field name='title' type='text' component={TextInput} placeholder='Give your event a name'/>
-                    <Field name='category' type='text' component={TextInput} placeholder='What is your event about'/>
-                    <Field name='description' type='text' component={TextInput} placeholder='Tell us about your event'/>
+                    <Field 
+                      name='title' 
+                      type='text' 
+                      component={TextInput} 
+                      placeholder='Give your event a name'
+                    />
+                    <Field 
+                      name='category' 
+                      type='text' 
+                      component={SelectInput} 
+                      options={category}
+                      placeholder='What is your event about'
+                    />
+                    <Field 
+                      name='description' 
+                      type='text' 
+                      row={3} 
+                      component={TextArea} 
+                      placeholder='Tell us about your event'
+                    />
                     <Header sub color='teal' content='Event Location Details'/>
                     <Field name='city' type='text' component={TextInput} placeholder='Event City'/>
                     <Field name='venue' type='text' component={TextInput} placeholder='Event Venue'/>
