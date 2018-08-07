6984e0f74736af00aa59f169d048d6cfc8f90476
Daniel Doyle <dbdoyle182@gmail.com>
2018-08-06 21:25:04 -0400

    events dashboard connected to redux

--- src/features/event/EventDetailed/EventDetailedPage.jsx
+++ src/features/event/EventDetailed/EventDetailedPage.jsx
@@ -4,33 +4,34 @@ import { EventDetailedChat } from './EventDetailedChat';
 import { EventDetailedInfo } from './EventDetailedInfo';
 import { EventDetailedSidebar } from './EventDetailedSidebar';
 import { Grid } from 'semantic-ui-react';
+import { connect } from 'react-redux';
 
-const event = {
-    id: '1',
-    title: 'Trip to Tower of London',
-    date: '2018-03-27',
-    category: 'culture',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
-    city: 'London, UK',
-    venue: "Tower of London, St Katharine's & Wapping, London",
-    hostedBy: 'Bob',
-    hostPhotoURL: 'https://randomuser.me/api/portraits/men/20.jpg',
-    attendees: [
-      {
-        id: 'a',
-        name: 'Bob',
-        photoURL: 'https://randomuser.me/api/portraits/men/20.jpg'
-      },
-      {
-        id: 'b',
-        name: 'Tom',
-        photoURL: 'https://randomuser.me/api/portraits/men/22.jpg'
-      }
-    ]
+const mapState = (state, ownProps) => {
+  const eventId = ownProps.match.params.id;
+
+  let event = {};
+
+  if (eventId && state.events.length > 0) {
+    event = state.events.filter(event => event.id === eventId)[0];
+  }
+
+
+  return {
+    event
+  }
 }
-export default class EventDetailedPage extends Component {
+class EventDetailedPage extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       
+    }
+  }
+  
   render() {
+    const { event } = this.props;
+
     return (
       <Grid>
         <Grid.Column width={10}>
@@ -45,3 +46,5 @@ export default class EventDetailedPage extends Component {
     )
   }
 }
+
+export default connect(mapState)(EventDetailedPage);
