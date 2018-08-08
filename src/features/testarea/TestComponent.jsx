d2ec57db61f98f530c90edcd3d9fe8600cd50555
Daniel Doyle <dbdoyle182@gmail.com>
2018-08-08 17:59:32 -0400

    set up redux thunk

--- src/features/testarea/TestComponent.jsx
+++ src/features/testarea/TestComponent.jsx
@@ -1,28 +1,29 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Button } from 'semantic-ui-react';
-import { incrementCounter, decrementCounter } from './testActions';
+import { incrementAsync, decrementAsync } from './testActions';
 import { openModal } from '../modals/modalActions';
 
 const mapState = (state) => ({
-  data: state.test.data
+  data: state.test.data,
+  loading: state.test.loading
 })
 
 const actions = {
-  incrementCounter,
-  decrementCounter,
+  incrementAsync,
+  decrementAsync,
   openModal
 }
 
 class TestComponent extends Component {
   render() {
-    const { decrementCounter, incrementCounter, data, openModal } = this.props
+    const { decrementAsync, incrementAsync, data, openModal, loading } = this.props
     return (
       <div>
         <h1>Test Area</h1>
         <h3> The answer is: {data}</h3>
-        <Button onClick={incrementCounter} color='green' content='+'/>
-        <Button onClick={decrementCounter} color='red' content='-'/>
+        <Button loading={loading} disabled={loading} onClick={incrementAsync} color='green' content='+'/>
+        <Button loading={loading} disabled={loading} onClick={decrementAsync} color='red' content='-'/>
         <Button onClick={() => openModal('TestModal', {data: 43})} color='teal' content='Open Modal'/>
       </div>
     )
