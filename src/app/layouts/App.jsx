a0ab52ae77306e8b5e4d9bd208e0d8eed2b425ab
Daniel Doyle <dbdoyle182@gmail.com>
2018-08-08 15:32:42 -0400

    redux modal structure created

--- src/app/layouts/App.jsx
+++ src/app/layouts/App.jsx
@@ -10,12 +10,14 @@ import PeopleDashboard from '../../features/user/PeopleDashboard/PeopleDashboard
 import UserDetailed from '../../features/user/UserDetailed/UserDetailedPage';
 import SettingsDashboard from '../../features/user/Settings/SettingsDashboard';
 import TestComponent from '../../features/testarea/TestComponent';
+import ModalManager from '../../features/modals/ModalManager';
 
 
 class App extends Component {
   render() {
     return (
       <div>
+        <ModalManager />
         <Switch>
           <Route exact path='/' component={HomePage} />
         </Switch>
