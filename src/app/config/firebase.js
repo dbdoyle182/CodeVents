bd7f58e748b5fd96095f2e2e3d212e28ad940ef7
Daniel Doyle <dbdoyle182@gmail.com>
2018-08-09 17:18:40 -0400

    adding firebase to the app

---
+++ src/app/config/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase';
+import 'firebase/firestore';
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDHOao_DzjWhtbhNxVQpidVL9kHv8WTuNw",
+    authDomain: "codevents-212615.firebaseapp.com",
+    databaseURL: "https://codevents-212615.firebaseio.com",
+    projectId: "codevents-212615",
+    storageBucket: "codevents-212615.appspot.com",
+    messagingSenderId: "497062315975"
+}
+
+firebase.initializeApp(firebaseConfig);
+firebase.firestore();
+
+export default firebase;
