be0783c9a9b0ecf20078d3091f6274836b218853
Daniel Doyle <dbdoyle182@gmail.com>
2018-08-09 18:16:16 -0400

    adjusted dates to format properly

--- src/app/config/firebase.js
+++ src/app/config/firebase.js
@@ -12,6 +12,11 @@ const firebaseConfig = {
 }
 
 firebase.initializeApp(firebaseConfig);
-firebase.firestore();
+const firestore = firebase.firestore();
+const settings = {
+    timestampsInSnapshots: true
+}
+
+firestore.settings(settings);
 
 export default firebase;
