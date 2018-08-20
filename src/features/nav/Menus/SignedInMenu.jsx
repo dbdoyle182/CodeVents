07dde3b7c5be3c0e86c3fa05d290938c2efa0afe
Daniel Doyle <dbdoyle182@gmail.com>
2018-08-20 11:38:44 -0400

    edited navbar

--- src/features/nav/Menus/SignedInMenu.jsx
+++ src/features/nav/Menus/SignedInMenu.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { Menu, Dropdown, Image } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
-export default ({signOut, auth}) => {
+export default ({signOut, profile}) => {
   return (
     <Menu.Item position="right">
-        <Image avatar spaced="right" src='/assets/user.png' />
-        <Dropdown pointing="top left" text={auth.email}>
+        <Image avatar spaced="right" src={profile.photoURL || '/assets/user.png'} />
+        <Dropdown pointing="top left" text={profile.displayName}>
         <Dropdown.Menu>
             <Dropdown.Item text="Create Event" icon="plus" />
             <Dropdown.Item text="My Events" icon="calendar" />
