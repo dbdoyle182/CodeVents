26a19c908c11028c3376a24b38f59e16caa451b6
Daniel Doyle <dbdoyle182@gmail.com>
2018-09-09 11:11:34 -0400

    finished photo module

--- src/features/user/Settings/PhotosPage.jsx
+++ src/features/user/Settings/PhotosPage.jsx
@@ -29,7 +29,8 @@ const actions = {
 const mapState = (state) => ({
     auth: state.firebase.auth,
     profile: state.firebase.profile,
-    photos: state.firestore.ordered.photos
+    photos: state.firestore.ordered.photos,
+    loading: state.async.loading
 })
 
 class PhotosPage extends Component {
@@ -98,7 +99,7 @@ class PhotosPage extends Component {
         })
     }
     render() {
-        const { photos, profile } = this.props;
+        const { photos, profile, loading } = this.props;
         let filteredPhotos;
         if (photos) {
             filteredPhotos = photos.filter(photo => {
@@ -146,8 +147,8 @@ class PhotosPage extends Component {
                                 <Image style={{minHeight: '200px', minWidth: '200px'}} src={this.state.cropResult}/>
 
                                 <Button.Group>
-                                    <Button onClick={this.uploadImage} style={{width: '100px'}} positive icon='check'/>
-                                    <Button onClick={this.cancelCrop} style={{width: '100px'}} icon='close'/>
+                                    <Button loading={loading} onClick={this.uploadImage} style={{width: '100px'}} positive icon='check'/>
+                                    <Button disabled={loading} onClick={this.cancelCrop} style={{width: '100px'}} icon='close'/>
                                 </Button.Group>
                             </div>
                         }
@@ -160,7 +161,7 @@ class PhotosPage extends Component {
 
                 <Card.Group itemsPerRow={5}>
                     <Card>
-                        <Image src={profile.photoURL} />
+                        <Image src={profile.photoURL || '/assets/user.png'} />
                         <Button positive>Main Photo</Button>
                     </Card>
                     {photos && filteredPhotos.map((photo) => (
